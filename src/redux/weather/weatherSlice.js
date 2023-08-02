9ec22b7162d5480f9b54603433fe03cfea78c2cc
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-08-03 01:33:04 +0430

    weather fetched and stored

---
+++ src/redux/weather/weatherSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getWeather = createAsyncThunk(
+  'countryStates/getWeather',
+  async (stateName) => {
+    const key = '71c3ae42ba7c4be4b58113417233107';
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${stateName}&days=3&aqi=yes`,
+    );
+    const data = await response.json();
+    return data.forecast.forecastday.map((day) => ({
+      province: data.location.region,
+      date: day.date,
+      condition: day.day.condition.text,
+      icon: day.day.condition.icon,
+      temp: day.day.avgtemp_c,
+      id: Date.now(),
+    }));
+  },
+);
+
+const initialState = {
+  weatherDetails: [],
+  isLoading: true,
+  hasError: false,
+};
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getWeather.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getWeather.fulfilled, (state, action) => {
+        state.weatherDetails = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getWeather.rejected, (state) => {
+        state.hasError = true;
+      });
+  },
+});
+
+export default weatherSlice.reducer;
